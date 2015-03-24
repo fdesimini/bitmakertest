2375651d9639abf89c10d0438bbe3c6020ced5a5
Frank Desimini <frankdesimini@Franks-MacBook-Air.local>
2015-03-24 14:32:09 -0400

    second time committing code

--- first-go/main.m
+++ first-go/main.m
@@ -11,6 +11,7 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         int firstInt=20;
+        int secondInt=40;
         // insert code here...
         NSLog(@"Hello, World!");
     }
