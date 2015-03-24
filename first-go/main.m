70e02a5540f1371fbf08e89ce2bf692f7fd76b17
Frank Desimini <frankdesimini@Franks-MacBook-Air.local>
2015-03-24 14:20:52 -0400

    Initial Commit

---
+++ first-go/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  first-go
+//
+//  Created by Frank Desimini on 2015-03-24.
+//  Copyright (c) 2015 Frank Desimini. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
